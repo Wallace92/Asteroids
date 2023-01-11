9fb31bb98ed684f0bcaf0dab8db7ba65b05478ec
Mateusz <mateusz.glinkowski@pwr.edu.pl>
2023-01-11 13:06:09 +0100

    Added Generic Spawner class for instantiating the T objects.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Spawner<T> : MonoBehaviour
+{
+    public T SpawnedPrefab;
+    protected T Spawn(GameObject spawnedPrefab)
+    {
+        GameObject go = Instantiate(spawnedPrefab.gameObject, new Vector3(0, 0, 0), Quaternion.identity);
+        T t = go.GetComponent<T>();
+        return t;
+    }
+}
