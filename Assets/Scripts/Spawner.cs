342e7f8f06c783a0c81ebba6f3dc04d281b7668b
Mateusz <mateusz.glinkowski@pwr.edu.pl>
2023-01-11 14:53:54 +0100

    [Scripts] Implement random position in Spawn method by serializable structure.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,12 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public struct SpawnBounds
+{
+    public float x;
+    public float y;
+    public float z;
+}
 
 public class Spawner<T> : MonoBehaviour
 {
-    public T SpawnedPrefab;
+    [Header("Spawner Settings")]
+    [SerializeField] protected T SpawnedPrefab;
+    [SerializeField] protected SpawnBounds SpawnBounds;
+    
     protected T Spawn(GameObject spawnedPrefab)
     {
-        GameObject go = Instantiate(spawnedPrefab.gameObject, new Vector3(0, 0, 0), Quaternion.identity);
-        T t = go.GetComponent<T>();
-        return t;
+        GameObject go = Instantiate(spawnedPrefab.gameObject, SpawnPos(), Random.rotation);
+        
+        return go.GetComponent<T>();
+    }
+
+    private Vector3 SpawnPos()
+    {
+        var x = Random.Range(-SpawnBounds.x, SpawnBounds.x);
+        var y = Random.Range(-SpawnBounds.y, SpawnBounds.y);
+        var z = Random.Range(-SpawnBounds.z, SpawnBounds.z);
+        return new Vector3(x, y, z);
     }
 }
